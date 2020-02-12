24a9d2f4b3cf1ecee53a77252af3572d03e3465b
zf188 <1976133549@qq. com>
2020-02-12 11:50:28 +0800

    test

--- src/Job/MyBookMain.java
+++ src/Job/MyBookMain.java
@@ -14,13 +14,43 @@ public class MyBookMain {
     public static void main(String[] args) {
         inputData(list);
         //print(list);
-        searchName(list,"书名9");
+        menu();
+
+ /*       searchName(list,"书名9");
         deleteName(list,"书名2");
         addBook(list);
-        print(list);
+        print(list);*/
     }
     public static void menu(){
-        System.out.println();
+        System.out.println("输入菜单选项");
+        System.out.println("1.添加图书");
+        System.out.println("2.删除图书");
+        System.out.println("3.查找图书");
+        System.out.println("4.查看所有");
+        System.out.println("5.退出");
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        switch (i){
+            case 1:
+                addBook(list);
+                break;
+            case 2:
+                System.out.println("请输入要删除的图书：");
+                String name = scanner.next();
+                deleteName(list,name);
+                break;
+            case 3:
+                System.out.println();
+                String searchName = scanner.next();
+                searchName(list,searchName);
+                break;
+            case 4:
+                print(list);
+                break;
+            default:
+                System.exit(1);
+                break;
+        }
     }
 
     public static void inputData(List<MyBook>books){
@@ -34,17 +64,21 @@ public class MyBookMain {
     public static void print(List<MyBook>books){
         for (int i = 0;i<books.size();i++){
             System.out.println(books.get(i));
+
         }
+        menu();
     }
     public static void searchName(List<MyBook> books, String name){
         for (int i = 0;i<books.size();i++){
             MyBook myBook = books.get(i);
             if(myBook.getName().equals(name)){
                 System.out.println(myBook);
+                menu();
                 return;
             }
         }
-        System.out.println("次数不存在");
+        System.out.println("此书不存在");
+        menu();
     }
     public static void deleteName(List<MyBook> books, String name){
         for (int i = 0;i<books.size();i++){
@@ -52,10 +86,12 @@ public class MyBookMain {
             if(myBook.getName().equals(name)){
                 books.remove(i);
                 System.out.println("删除成功");
+                menu();
                 return;
             }
         }
-        System.out.println("次数不存在");
+        System.out.println("此书不存在");
+        menu();
     }
     public static void addBook(List<MyBook>books){
         Scanner scanner = new Scanner((System.in));
@@ -71,5 +107,6 @@ public class MyBookMain {
         String bookISBN = scanner.next();
         MyBook myBook = new MyBook(name,price,press,author,bookISBN);
         books.add(myBook);
+        menu();
     }
 }
